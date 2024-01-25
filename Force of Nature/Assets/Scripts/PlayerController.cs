85e094486b4ff8f54e358ca811a8c15fb6245432
oolst71 <115809707+oolst71@users.noreply.github.com>
2024-01-25 23:42:33 +0100

    Dash fully functional
    Dash working fairly well right now

--- Force of Nature/Assets/Scripts/PlayerController.cs
+++ Force of Nature/Assets/Scripts/PlayerController.cs
@@ -8,22 +8,27 @@ public class PlayerController : MonoBehaviour
     [SerializeField]float moveSpeed = 5f;
     [SerializeField] float jumpSpeed = 3f;
     [SerializeField] float dashPower = 50f;
-    [SerializeField] float dashTime = 0.2f;
-    [SerializeField] float dashCooldown = 1.5f;
+    [SerializeField] float dashDuration = 0.2f;
+    [SerializeField] float dashCooldown = 0.5f;
     [SerializeField] float coyoteTime = 0.1f;
 
     private Rigidbody2D rb;
     private CapsuleCollider2D coll;
 
+    private float baseGravity;
     private float movementInput; //the horizontal axis input of the player
     private Vector2 aim; //the stick input of the player
     private Vector2 dashDir;
+    private float faceDir; //the direction the player is facing
 
     private LayerMask ground;
     [SerializeField]private bool grounded;
     private bool dashCd;
     private bool dashing;
+
     private float coyoteTimer;
+    private float dashCooldownTimer;
+    private float dashDurationTimer;
 
     //ok so these will need to be reworked whenever i figure out a better way to do this, because there's gotta be a better way to do this
     private bool canMove; //set this to false to make the player completely unable to move by themselves (for cutscenes, for example)
@@ -42,7 +47,9 @@ public class PlayerController : MonoBehaviour
         canDash = true;
         dashCd = true;
         dashing = false;
+        baseGravity = rb.gravityScale;
         coyoteTimer = 0f;
+        dashCooldownTimer = 100f;
     }
 
     void FixedUpdate()
@@ -51,7 +58,9 @@ public class PlayerController : MonoBehaviour
         {
             rb.velocity = new Vector2(aim.x * moveSpeed, rb.velocity.y);
         }
+
         grounded = GroundCheck();
+
         if (!grounded)
         {
             coyoteTimer += Time.deltaTime;
@@ -60,6 +69,16 @@ public class PlayerController : MonoBehaviour
         {
             coyoteTimer = 0;
         }
+
+        if (dashing)
+        {
+            ProcessDash();
+        }
+        else
+        {
+            dashCooldownTimer += Time.deltaTime;
+        }
+
     }
 
     private void OnJump()
@@ -73,6 +92,10 @@ public class PlayerController : MonoBehaviour
     private void OnStickInput(InputValue value)
     {
         aim = value.Get<Vector2>();
+        if (aim.x != 0)
+        {
+            faceDir = Mathf.Sign(aim.x);
+        }
     }
 
    private void OnTestAxis(InputValue value)
@@ -82,17 +105,26 @@ public class PlayerController : MonoBehaviour
 
     private void OnDash()
     {
-        if (canDash && canMove && dashCd)
+        if (canDash && canMove && dashCooldownTimer > dashCooldown)
         {
-            dashDir = aim.normalized;
-
-            StartCoroutine(Dash());
+            dashDir = aim;
+            Debug.Log("aiming " + dashDir.x + " " + dashDir.y);
+            if (aim.y < 0.35f && aim.y > -0.35f)
+            {
+                dashDir.y = 0f;
+                if (aim.x == 0f)
+                {
+                    dashDir.x = faceDir;
+                }
+            }
+            Debug.Log("dashing " + dashDir.x + " " + dashDir.y);
+            dashDir.Normalize();
+            StartDash();
         }
     }
 
     private bool GroundCheck()
     {
-        Debug.Log("gc");
         //boxcast directly downwards
         //if it hits the "ground" layermask (which contains anything the player can stand on), it counts as being grounded
         if (Physics2D.BoxCast(gameObject.transform.position, 0.95f * coll.bounds.size, 0f, Vector2.down, 0.2f, ground))
@@ -105,19 +137,29 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    private IEnumerator Dash()
+    private void StartDash()
     {
-        dashCd = false;
         canMove = false;
-        float ogGravity = rb.gravityScale;
         rb.gravityScale = 0f;
         rb.velocity = new Vector2(dashDir.x * dashPower, dashDir.y * dashPower);
-        yield return new WaitForSeconds(dashTime);
-        rb.gravityScale = ogGravity;
-        rb.velocity = Vector2.zero;
-        canMove = true;
-        dashing = false;
-        yield return new WaitForSeconds(dashCooldown);
-        dashCd = true;
+        dashing = true;
+        dashDurationTimer = 0f;
+    }
+
+    private void ProcessDash()
+    {
+        if (dashDurationTimer > dashDuration) //end dash
+        {
+            dashCooldownTimer = 0f;
+            rb.velocity = Vector2.zero;
+            rb.gravityScale = baseGravity;
+            canMove = true;
+            dashing = false;
+            rb.velocity = Vector2.zero;
+            dashCooldownTimer = 0f;
+        } else
+        {
+            dashDurationTimer += Time.deltaTime;
+        }
     }
 }
