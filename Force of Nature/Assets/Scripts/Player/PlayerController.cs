5eb057ef228a6a10b3d41341135a3a5d2f2ea169
soarsoso <sophiabieringer0409@gmail.com>
2024-05-09 13:56:25 +0200

    Revert "Merge branch 'main' into uwu"
    This reverts commit 366cbfa029fa3e30791bf5bc1018ad27693d43ea, reversing
    changes made to 6a5f0c15d66cc5b85b0c82ac89fe354f3a57724d.

--- Force of Nature/Assets/Scripts/Player/PlayerController.cs
+++ Force of Nature/Assets/Scripts/Player/PlayerController.cs
@@ -12,7 +12,6 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rb;
     private BoxCollider2D coll;
     private TrailRenderer trail;
-    private SpriteRenderer sprite;
 
     public Vector2 aim; //the stick input of the player
     private Vector2 dashDir;
@@ -34,7 +33,6 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         coll = GetComponent<BoxCollider2D>();
         trail = GetComponent<TrailRenderer>();
-        sprite = GetComponent<SpriteRenderer>();
         trail.emitting = false;
         ground = LayerMask.GetMask("Platform"); //here you put in any layers that the player can step on
         playerData.currentState = PlayerDataScrObj.playerState.IDLE;
@@ -167,7 +165,6 @@ public class PlayerController : MonoBehaviour
         {
             faceDir = Mathf.Sign(aim.x);
             playerData.faceDir = faceDir;
-            sprite.transform.localScale = new Vector3(-faceDir, 1, 1);
         }
     }
 
